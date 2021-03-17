05ae49b1d6d207baa6cab7ed86e856a8585d4e2a
korawan <korawan.ser@ku.th>
2021-03-17 16:13:27 +0700

    add put,delete,getByusername

--- index.js
+++ index.js
@@ -63,6 +63,84 @@ app.post('/user', (req, res) => {
     }
 })
 
+//retrieve by username
+app.get('/user/:username', (req, res) => {
+    let username = req.params.username;
+
+    if(!username){
+        return res.status(400).send({error: true, message: "Please provide username"})
+    }
+    else{
+        dbCon.query("SELECT * FROM user WHERE username = ?", username, (error, results, fields) => {
+            if(error) throw error;
+
+            let message = "";
+            if(results === undefined || results.length == 0) {
+                message = "username not found";
+            }
+            else{
+                message = "Successfully retrieved user data";
+            }
+            return res.send({error: false, data: results[0], message: message})
+        })
+    }
+})
+
+//update user by username
+app.put('/user', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if(!username || !password){
+        return res.status(400).send({error: true, message: "Please provide username and password"})
+    }
+    else
+    {
+        dbCon.query('UPDATE user SET password = ? WHERE username = ?', [password,username], (error, results, fields) => {
+            if(error) throw error;
+
+            let message = "";
+            if(results.changRows === 0) {
+                message = "user not found or data same";
+            }
+            else
+            {
+                message = "user successfully updated";
+            }
+
+            return res.send({ error: false, data: results, message: message})
+        })
+    }
+})
+
+//DELETE user by username
+app.delete('/user', (req, res) => {
+    let username = req.body.username;
+
+    
+    if(!username){
+        return res.status(400).send({error: true, message: "Please provide username"})
+    }
+    else
+    {
+        dbCon.query('DELETE FROM user WHERE username = ?', username, (error, results, fields) => {
+            if(error) throw error;
+
+            let message = "";
+            if(results.affectedRows === 0) {
+                message = "user not found";
+            }
+            else
+            {
+                message = "user successfully deleted";
+            }
+
+            return res.send({ error: false, data: results, message: message})
+        })
+    }
+
+})
+
 app.listen(5001, () => {
     console.log('Node App run on port 5001');
 })
